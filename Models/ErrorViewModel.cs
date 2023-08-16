2ebd866aa7e9ae916039ffa1edebb2e5430b37fe
AlexandraKulikowa <siriusblack@yandex.ru>
2023-08-16 14:53:34 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DepartmentsWEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
